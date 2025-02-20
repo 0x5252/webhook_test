c46f6f3a08ad357d21e21bc4c26fc74728ed2753
0x5252 <ruben812000@gmail.com>
2025-02-20 10:36:40 +0100

    Simple test case

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+simple webhook using flask
+"""
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    print("webhook called")
+    data = request.get_json()
+    print(data)
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5002)
